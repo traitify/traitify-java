ddb30ca878324e458f245af5bc8bae77e24e1432
Eric Fleming <efleming@gmail.com>
2014-06-13 12:02:30 -0400

    Changes to decks to return badges.

--- src/main/java/com/traitify/models/Deck.java
+++ src/main/java/com/traitify/models/Deck.java
@@ -20,17 +20,11 @@ public class Deck extends ApiModel {
 
     private String description;
 
-    private String personality_group_id;
-
-    private String personality_group_name;
-
-    private Date created_at;
+    private String personality_group;
 
     private Integer slide_count;
-    
-    private Boolean active;
 
-    private PersonalityGroup personality_group;
+    private List<DeckBadge> badges;
 
     public String getId() {
         return id;
@@ -56,28 +50,12 @@ public class Deck extends ApiModel {
         this.description = description;
     }
 
-    public String getPersonality_group_id() {
-        return personality_group_id;
+    public String getPersonality_group() {
+        return personality_group;
     }
 
-    public void setPersonality_group_id(String personality_group_id) {
-        this.personality_group_id = personality_group_id;
-    }
-
-    public String getPersonality_group_name() {
-        return personality_group_name;
-    }
-
-    public void setPersonality_group_name(String personality_group_name) {
-        this.personality_group_name = personality_group_name;
-    }
-
-    public Date getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(Date created_at) {
-        this.created_at = created_at;
+    public void setPersonality_group(String personality_group) {
+        this.personality_group = personality_group;
     }
 
     public Integer getSlide_count() {
@@ -88,19 +66,11 @@ public class Deck extends ApiModel {
         this.slide_count = slide_count;
     }
 
-    public Boolean getActive() {
-        return active;
+    public List<DeckBadge> getBadges() {
+        return badges;
     }
 
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public PersonalityGroup getPersonality_group() {
-        return personality_group;
-    }
-
-    public void setPersonality_group(PersonalityGroup personality_group) {
-        this.personality_group = personality_group;
+    public void setBadges(List<DeckBadge> badges) {
+        this.badges = badges;
     }
 }
---
+++ src/main/java/com/traitify/models/DeckBadge.java
@@ -0,0 +1,15 @@
+package com.traitify.models;
+
+public class DeckBadge extends Badge {
+
+    private String personality_type;
+
+    public String getPersonality_type() {
+        return personality_type;
+    }
+
+    public void setPersonality_type(String personality_type) {
+        this.personality_type = personality_type;
+    }
+
+}

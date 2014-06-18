c07fe475f14be2052558ce577a0b998334bf4588
Eric Fleming <efleming@gmail.com>
2014-06-18 14:43:08 -0700

    Updating personality types to return compatible personality types and famous people

--- src/main/java/com/traitify/models/PersonalityType.java
+++ src/main/java/com/traitify/models/PersonalityType.java
@@ -2,13 +2,18 @@ package com.traitify.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PersonalityType {
 
     private String id;
     private String name;
     private String description;
+    private String compatible_personality_type_id;
     private Badge badge;
+
+    private List<FamousPerson> famous_people;
 
     public String getId() {
         return id;
@@ -40,6 +45,22 @@ public class PersonalityType {
 
     public void setBadge(Badge badge) {
         this.badge = badge;
+    }
+
+    public String getCompatible_personality_type_id() {
+        return compatible_personality_type_id;
+    }
+
+    public void setCompatible_personality_type_id(String compatible_personality_type_id) {
+        this.compatible_personality_type_id = compatible_personality_type_id;
+    }
+
+    public List<FamousPerson> getFamous_people() {
+        return famous_people;
+    }
+
+    public void setFamous_people(List<FamousPerson> famous_people) {
+        this.famous_people = famous_people;
     }
 
 }
